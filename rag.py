1691f29f1a13077522623c981b329f0ef15643e6
Aryan Sharma <100895725+HellDragger@users.noreply.github.com>
2024-11-04 19:12:28 +0530

    frontend

--- rag.py
+++ rag.py
@@ -1,7 +1,7 @@
 import transformers
-from langchain.llms import HuggingFacePipeline
+from langchain_community.llms import HuggingFacePipeline
 from langchain.prompts import PromptTemplate
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.chains import LLMChain
 import chromadb
 import nest_asyncio
